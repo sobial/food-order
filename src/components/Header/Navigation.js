ccb893f74cb3dd68d7be068f7c5385d12b0505ea
sobial <sobhaniali1@gmail.com>
2021-08-17 16:10:19 +0430

    navigation

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,4 +1,10 @@
+import styles from "./Navigation.module.css";
 const Navigation = (props) => {
-  return <div>nav here</div>;
+  return (
+    <div className={styles.nav_bar}>
+      <h1>ReactMeals</h1>
+      <button>Cart</button>
+    </div>
+  );
 };
 export default Navigation;
