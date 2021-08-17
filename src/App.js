fc851609334fc9f716e65d5f71a18de4f32b8407
sobial <sobhaniali1@gmail.com>
2021-08-17 17:09:16 +0430

    foodList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import "./App.css";
 import Header from "./components/Header/Header";
 import FoodList from "./components/FoodList";
+import styles from "./App.module.css";
+
+const flist = [
+  { name: "kabab", id: "f1" },
+  { name: "abgoosht", id: "f2" },
+  { name: "gheime", id: "f3" },
+  { name: "jooje", id: "f4" },
+  { name: "an", id: "f5" },
+];
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Header />
-      <FoodList />
+      <main className={styles.main}>
+        <FoodList meals={flist} />
+      </main>
     </div>
   );
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,4 +1,27 @@
+import Card from "./UI/Card";
+import styles from "./FoodList.module.css";
+import React from "react";
+
 const FoodList = (props) => {
-  return <div>list of foods</div>;
+  return (
+    <React.Fragment>
+      <Card className={styles.discription}>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a
+          metus a elit bibendum euismod. Praesent aliquet libero in maximus
+          mattis. Ut metus dolor, egestas et porttitor vel, varius vel nisi.
+          Maecenas non arcu vestibulum, semper nulla sit amet, sagittis nisi.
+          Proin egestas ipsum maximus mi elementum iaculis.
+        </p>
+      </Card>
+
+      <Card className={styles.list_card}>
+        {props.meals.map((meal) => {
+          return <div key={meal.id}>{meal.name}</div>;
+        })}
+      </Card>
+    </React.Fragment>
+  );
 };
 export default FoodList;
