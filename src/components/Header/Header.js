5c832aa4d3a4c6a16bd9216eba3919471132ceba
sobial <sobhaniali1@gmail.com>
2021-08-17 16:10:10 +0430

    header image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import Card from "../UI/Card";
 import Navigation from "./Navigation";
 import styles from "./Header.module.css";
+import headerImg from "../../header.jpg";
 
 const Header = () => {
   return (
     <div>
       <Navigation />
-      <div className={styles.img_shape}>here the image</div>
-      <Card className={styles.card}>here the discription card</Card>
+      <div className={styles.img_shape}>
+        <img src={headerImg} alt="this is dummy pic" />
+        <Card className={styles.card}>here the discription card</Card>
+      </div>
     </div>
   );
 };
